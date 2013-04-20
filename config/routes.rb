b8274beac4d39d1a6d447553d361515c35ad3f62
Laura Lopez <zepolveda@gmail.com>
2013-04-20 16:56:05 -0700

    Added Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Omrails::Application.routes.draw do
+  get 'about' => 'pages#about' #creates about route pointing to pages action
+
+  root :to => 'pages#home'
+ 
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
